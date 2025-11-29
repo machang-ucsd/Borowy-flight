ed7dc04b1d70e935ddfac2040a5e6eef4db9a8b3
Maxwell Chang <maxwellchang@Maxwells-MacBook-Pro.local>
2025-11-29 01:11:26 -0800

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // IMPORTANT: The name inside the quotes must match your GitHub Repo name EXACTLY.
   // Your repo is "Borowy-flight", so the base must be "/Borowy-flight/"
-  base: "/Borowy-flight/", 
+  base: "/", 
 })
