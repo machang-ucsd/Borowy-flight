05cedf5b94becfd3ee2a4c27b45fa71b693c8d5b
Maxwell Chang <maxwellchang@Maxwells-MacBook-Pro.local>
2025-11-28 22:38:09 -0800

    Changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or the plugin for your framework
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], // or your framework's plugin
-  base: '/borowy-flight/', // 👈 Add this line
+  plugins: [react()],
+  // IMPORTANT: The name inside the quotes must match your GitHub Repo name EXACTLY.
+  // Your repo is "Borowy-flight", so the base must be "/Borowy-flight/"
+  base: "/Borowy-flight/", 
 })
